077f01efb27a1262b06e71859e879892b5d4753e
Gabriel Cruz <gabriel.cruzjoa@gmail.com>
2021-03-17 18:31:38 -0400

    Using a Sports API

--- Using_APIs/Views/MainPage.xaml.cs
+++ Using_APIs/Views/MainPage.xaml.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
-using System.Net;
-using System.Net.Http;
-using Newtonsoft.Json;
 using Using_APIs.ViewModels;
 using Using_APIs.Services;
 
