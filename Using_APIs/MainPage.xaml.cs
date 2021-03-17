54c80322b9d3cc6b81e2c3ecc7107e8c063417f4
Gabriel Cruz <gabriel.cruzjoa@gmail.com>
2021-03-16 23:41:04 -0400

    Add project files.

---
+++ Using_APIs/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Using_APIs
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public class DemoApi
+        {
+            public int userId { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+            public string body { get; set; }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://sportscore1.p.rapidapi.com/teams"),
+                Headers =
+                {
+                    { "x-rapidapi-key", "4ea8d7e6f9mshd31033cdadc032bp1b5777jsn07633bf1455a" },
+                    { "x-rapidapi-host", "sportscore1.p.rapidapi.com" },
+                },
+            };
+
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                //var result = JsonConvert.DeserializeObject<List<DemoApi>>(content);
+                var result = JsonConvert.DeserializeObject<Team>(content).data.ToList();
+
+                //var result = JsonConvert.DeserializeObject<Sport>(content).ToString();
+
+                ListDemo.ItemsSource = result;
+            }
+
+        }
+
+
+    }
+}
---
+++ Using_APIs/Team.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Using_APIs
+{
+    public class NameTranslations
+    {
+        public string en { get; set; }
+        public string ru { get; set; }
+        public string de { get; set; }
+        public string es { get; set; }
+        public string fr { get; set; }
+        public string zh { get; set; }
+        public string tr { get; set; }
+        public string el { get; set; }
+        public string it { get; set; }
+        public string nl { get; set; }
+        public string pt { get; set; }
+    }
+
+    public class Details
+    {
+        public string country { get; set; }
+        public string country_i_s_o { get; set; }
+        public string date_of_birth { get; set; }
+        public string birthplace { get; set; }
+        public string plays { get; set; }
+        public string residence { get; set; }
+        public string weight { get; set; }
+        public double height_meters { get; set; }
+        public string prize_current { get; set; }
+        public string prize_total { get; set; }
+        public int prize_current_euros { get; set; }
+        public int prize_total_euros { get; set; }
+        public string flag { get; set; }
+    }
+
+    public class Sport
+    {
+        public int id { get; set; }
+        public string slug { get; set; }
+        public string name { get; set; }
+    }    
+
+    public class Datum
+    {
+        public int id { get; set; }
+        public string slug { get; set; }
+        public string name { get; set; }
+        public NameTranslations name_translations { get; set; }
+        public string name_short { get; set; }
+        public string name_full { get; set; }
+        public string name_code { get; set; }
+        public bool has_sub { get; set; }
+        public bool has_logo { get; set; }
+        public string logo { get; set; }
+        public string gender { get; set; }
+        public bool is_nationality { get; set; }
+        public string country_code { get; set; }
+        public string country { get; set; }
+        public string flag { get; set; }
+        public string foundation { get; set; }
+        public Details details { get; set; }
+        public Sport sport { get; set; }
+    }
+
+    public class Team
+    {
+        public IList<Datum> data { get; set; }
+    }
+}
