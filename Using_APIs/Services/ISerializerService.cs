fa2e7f9bee9e22423c41b569b57576b5fd78747d
Gabriel Cruz <gabriel.cruzjoa@gmail.com>
2021-03-17 20:21:17 -0400

    Adding Serialize Service

---
+++ Using_APIs/Services/ISerializerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Using_APIs.Services
+{
+    public interface ISerializerService
+    {
+        string Serialize(object data);
+       T Deserialize<T>(string DesData);
+    }
+}
--- Using_APIs/ViewModels/BaseViewModel.cs
+++ Using_APIs/ViewModels/BaseViewModel.cs
@@ -5,7 +5,6 @@ namespace Using_APIs.ViewModels
 {
     public abstract class BaseViewModel : INotifyPropertyChanged
     {
-        protected INavigationService NavigationService { get; }
         protected IAlertService AlertService { get; }
 
         protected BaseViewModel(IAlertService alertService)
