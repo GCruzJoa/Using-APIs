36ac279c8b71356b78823f40d3dc6b91d63a40ad
Gabriel Cruz <gabriel.cruzjoa@gmail.com>
2021-03-17 22:11:34 -0400

    Refactored Models

--- Using_APIs/Models/Sport.cs
+++ Using_APIs/Models/Sport.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +7,14 @@ namespace Using_APIs.Models
 {
     public class Sport
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+
+        [JsonProperty("slug")]
         public string Slug { get; set; }
+
+        [JsonProperty("name")]
         public string Name { get; set; }
     }
+
 }
--- Using_APIs/Models/Team.cs
+++ Using_APIs/Models/Team.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.ObjectModel;
+﻿using Newtonsoft.Json;
+using System.Collections.ObjectModel;
 using Using_APIs.Models;
 
 namespace Using_APIs
 {
     public class Team
     {
+        [JsonProperty("data")]
         public ObservableCollection<Datum> Data { get; set; }
     }
 }
